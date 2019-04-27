3085bb2d6c3f89fb0b189e43abdb2b491e4d0eb8
patsi97 <48284682+patsi97@users.noreply.github.com>
2019-04-27 10:43:50 +0200

    promjena 3. player dodan

--- main.cpp
+++ main.cpp
@@ -38,23 +38,51 @@ void move(int& x, int& y, int& velocity) {
 int main()
 {
 	Player playerOne = Player(0, 0, 10, 30);
-	playerOne.userName = "Chaos";
+	playerOne.userName = "Player1";
 	playerOne.strength = 12;
 
 	Player playerTwo = Player(10, 15, 10, 30);
-	playerTwo.userName = "Robi256";
+	playerTwo.userName = "Player2";
 	playerTwo.strength = 11;
 
-
+	Player playerThree = Player(0, 0, 10, 30);
+	playerThree.userName = "Player3";
+	playerThree.strength = 12;
+	
+	
 	playerOne.move();
 	playerTwo.move();
+	playerThree.move();
 
-	std::string losingPLayer;
+	std::string losingPlayer;
 	std::string winningPlayer;
-
-	while (playerOne.hp > 0 && playerTwo.hp > 0) 
+	
+	int playerNumber;
+	std:: string username;
+	std::cout<< "Enter which player you want to be (1,2,3) and enter your username: ";
+	
+	std::cin >> playerNumber;
+	std::cin >> username;
+	
+			switch (playerNumber)
+		{
+		case 1:
+			std::cin >> playerOne.userName;
+			break;
+		case 2:
+			playerTwo.userName = username;
+			break;
+		case 3:
+			playerThree.userName = username;
+			break;
+		default:
+			break;
+		
+}
+	while (playerOne.hp > 0 && playerTwo.hp > 0 && playerThree.hp > 0) 
 	{
 		int check;
+		
 		std::cout << "Chose which player you want to hit: ";
 		std::cin >> check;
 		std::cout << std::endl;
@@ -66,31 +94,39 @@ int main()
 		{
 		case 1:
 			playerOne.damage(playerTwo.strength);
-			std::cout << "Player one lost 5 hitpoints" << std::endl;
+			std::cout << "Player " << playerOne.userName <<" lost 5 hitpoints" << std::endl;
 			break;
 		case 2:
 			playerTwo.damage(playerOne.strength);
-			std::cout << "Player two lost 5 hitpoints" << std::endl;
+			std::cout << "Player "<< playerTwo.userName <<" lost 5 hitpoints" << std::endl;
 			break;
+		case 3:
+			playerThree.damage(playerThree.strength);
+			std::cout << "Player "<< playerThree.userName <<" lost 5 hitpoints" << std::endl;
 		default:
 			break;
 		}
-		std::cout << "Player one: " << playerOne.hp << std::endl;
-		std::cout << "Player two: " << playerTwo.hp << std::endl;
+		std::cout << "Player " << playerOne.userName <<  " hp: " << playerOne.hp << std::endl;
+		std::cout << "Player " << playerTwo.userName <<  " hp: " << playerTwo.hp << std::endl;
+		std::cout << "Player " << playerThree.userName <<  " hp: " << playerThree.hp << std::endl;
 		std::cout << std::endl;
 
 		if (playerOne.hp <= 0) {
-			losingPLayer = playerOne.userName;
+			losingPlayer = playerOne.userName;
 			winningPlayer = playerTwo.userName;
 		}
 		else if (playerTwo.hp <= 0) {
-			losingPLayer = playerTwo.userName;
+			losingPlayer = playerTwo.userName;
+			winningPlayer = playerOne.userName;
+		}else if(playerThree.hp <=0){
+			losingPlayer = playerThree.userName;
 			winningPlayer = playerOne.userName;
 		}
 	}
 
 	std::cout << winningPlayer << "  wins! Congratulations! " << std::endl;
-	std::cout << losingPLayer << "  lost! Better luck next time! " << std::endl;
+	std::cout << losingPlayer << "  lost! Better luck next time! " << std::endl;
+
 
 	//napraviti treceg playera, te postaviti moguænost za biranje usernamea(korisnik sam unosi username)
 	//prosiriti moguænost damaganja playera tako da se bira koji æe kojeg udariti;
@@ -98,3 +134,4 @@ int main()
 
 	std::cin.get();
 }
+
