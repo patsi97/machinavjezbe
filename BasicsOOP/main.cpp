72b180becc01d46afba9e589c683f4eabd61c468
patsi97 <48284682+patsi97@users.noreply.github.com>
2019-06-09 20:33:05 +0200

    Polymorphism Excercise

--- BasicsOOP/main.cpp
+++ BasicsOOP/main.cpp
@@ -2,11 +2,14 @@
 #include <string>
 #include <vector>
 
-//abstrakcija 
-//polimorfizam 
+//Polimorfizam radi samo na heapu! Ne na stacku! Omoguæuje transformaciju objekata iste hijerarhije,
+//da možemo lakše grupirati podatke
 //enkapsulacija 
 //nasljeðivanje
-class Human
+//pure virtual function - funckija bez implementacije, used for interface
+
+
+/*class Human
 {
 public:
 
@@ -18,6 +21,8 @@ public:
 	{
 		std::cout << "I am a human being!" << std::endl;
 	}
+	//pure virtual funtion - has no implementation, no instance
+	virtual int getNumberOfHands() = 0;
 
 private:
 
@@ -30,6 +35,12 @@ public:
 	{
 		std::cout << "I am a male human being!" << std::endl;
 	}
+	//virtual funtion implemented here
+	int getNumberOfHands() 
+	{
+		return hands;
+	}
+
 };
 
 class Female : public Human
@@ -39,37 +50,157 @@ public:
 	{
 		std::cout << "I am a female human being!" << std::endl;
 	}
+	int getNumberOfHands() {
+		return hands;
+	}
+	
 };
 
 
 int main()
 {
+	//Pointer na memoriju heapa, ne èisti se nakon izlaska iz ove funkcije
+	//memory leak - we don't have pointer on memory adress, cant access
 	Male* maleObject = new Male();
 	
 	maleObject->hands = 2;
 	maleObject->feet = 2;
 	maleObject->eyes = 2;
 
+	maleObject->PrintType();
 	Female* femaleObject = new Female();
 
 	femaleObject->PrintType();
-	maleObject->PrintType();
 	
-
+	
 	//zbog nasljeðivanja imamo ovu moguænost, ali æe se ispisati human object, a ne female object
-	Human* humanObject = new Human();
-	humanObject->PrintType();
+	//Human* humanObject = new Human();
+	//humanObject->PrintType();
 	
 	std::vector<Human*> humans;
 	humans.push_back(maleObject);
 	humans.push_back(femaleObject);
-	humans.push_back(humanObject);
+	//humans.push_back(humanObject);
 
 	std::cout << "===============================" << std::endl;
 	for (size_t i = 0; i < humans.size(); i++)
 	{
 		humans[i]->PrintType();
+		std::cout << humans[i]->getNumberOfHands() << std::endl;
 	}
 	std::cin.get();
+	//good practice to delete from heap on the end of function
+	delete femaleObject;
+	delete maleObject;
+}
+*/
 
+//ZADATAK:
+//Napraviti interface Shape koji ima metode Area i Size 
+//Napraviti klase Triangle, Circle, Square koji implementiraju interface Shape
+//Napraviti objekte svake podklase i ispisati Area i Size!
+
+class Shape
+{
+public:
+	
+	virtual double getArea() = 0; 
+	virtual double getPerimeter() = 0;
+	virtual std::string getName() = 0;
+private:
+	 
+};
+
+class Triangle:public Shape
+{
+public:
+	
+	float a, b, c;
+
+	double getPerimeter() {
+		return a+b+c;
+	}
+	double getArea() {
+		double s = getPerimeter()/2;
+		return sqrt(s*(s - a)*(s - b)*(s - c));
+	}
+	std::string getName() {
+		return "Triangle";
+	}
+};
+
+class Circle: public Shape 
+{
+public:
+	
+
+	float r;
+	double getPerimeter() {
+		return 2*r*_Pi;
+	}
+	double getArea() {
+		
+		return r*r*_Pi/2;
+	}
+	std::string getName() {
+		return "Circle";
+	}
+private:
+	float _Pi = 3.14;
+};
+
+class Rectangle: public Shape 
+{
+public:
+	float a, b;
+
+	
+	double getPerimeter() {
+		return 2 * a + 2 * b;
+	}
+	double getArea() {
+		return a * b;
+	}
+	std::string getName() {
+		return "Rectangle";
+	}
+};
+
+void printShapeInfo(Shape* shape) {
+	
+		std::cout << "Shape name " << shape->getName() << std::endl;
+		std::cout << "Area " << shape->getArea() << std::endl;
+		std::cout << "Perimeter " << shape->getPerimeter() << std::endl;
+		std::cout << "=============================" << std::endl;
+	}
+
+
+
+int main() {
+
+	
+	Rectangle* rectangleObject = new Rectangle();
+	rectangleObject->a = 5.0f;
+	rectangleObject->b = 7.0f;
+
+	Circle* circleObject = new Circle();
+	circleObject->r = 10.0f;
+
+	Triangle* triangleObject = new Triangle();
+	triangleObject->a = 5.0f;
+	triangleObject->b = 7.0f;
+	triangleObject->c = 9.0f;
+	
+	std::vector<Shape*> shapes;
+	shapes.push_back(rectangleObject);
+	shapes.push_back(circleObject);
+	shapes.push_back(triangleObject);
+	
+	for (Shape* i : shapes)
+	{
+		printShapeInfo(i);
+	}
+
+	std::cin.get();
+	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -2,172 +2,44 @@
 #include <string>
 #include <vector>
 
-
-//po defaultu visibility klase je private!
-//
-//po defaultu visibility structure je public!
-//kontejner podataka i manipulacija sa specifiènim podacima
-/*class Entity 
+//abstrakcija - 
+//polimorfizam - 
+//enkapsulacija - 
+class Human
 {
 public:
-	Vector3 position;
-private:
-	std::string name;
-
-};
-
-struct Vector3 
-{
-	float x, y, z;
-};
-
-class Player : public Entity
-{
-public:
-
-	Vector3 getPosition() {
-		return position;
-	}
-
-};
-
-class Enemy : public Entity
-{
-public:
-	Vector3 getPosition() {
-		return position;
-	}
-
-};
-*/
-
-
-class Person {
-
-public:
-	Person(std::string givenName, std::string givenLastName, std::string givenAdress) {
-		_name = givenName;
-		_lastName = givenLastName;
-		_adress = givenAdress;
-	}
-	~Person();
-
-	bool trySetSSN(std::string ssn) {
-		if (_socialSecurityNumber != "") {
-			std::cout << "Social security Number is already set!" << std::endl;
-			std::cout << "Persons SSN cannot be changed!" << std::endl;
-			return true;
-		}
-		if (ssn.size() != 11) {
-			std::cout << "Social security number is invalid! Wrong number of characters!";
-			return false;
-		}
-		for (size_t i = 0; i < ssn.size(); i++)
-		{
-			char current = ssn[i];
-			if (current <= 48 || current => 57) {
-				std::cout << "Social security number is invalid!" << std::endl;
-				std::cout << "Character at index" << i << "is invalid" << std::endl;
-				return false;
-			}
-		}
-		_socialSecurityNumber = ssn;
-		return true;
-	}
-
-	void print()
-	{
-		std::cout << "Name:" << _name << " " << _lastName << std::endl;
-		std::cout << "Adress" << _adress << std::endl;
-		std::cout << "Social security number" << _socialSecurityNumber << std::endl;
-	}
-
-private:
-	std::string _name, _lastName, _adress, _socialSecurityNumber;
-};
-
-void displayPersons(std::vector<Person> persons) {
-	for (size_t i = 0; i < persons.size(); i++)
-	{
-		persons[i].print();
-	}
-	}
-
-void createPerson(std::vector<Person>& persons) {
-	std::string name, lastName, adress;
-
-	inputNewData("Enter new persons last name:", name);
-	inputNewData("Enter new persons last name:", lastName);
-	inputNewData("Enter new persons last name:", adress);
-
-
-	Person person = Person(name, lastName, adress);
-
-	while (true)
-	{
-		std::string ssn;
-		inputNewData("Enter new social security number: ", ssn);
-		
-		if (person.trySetSSN(ssn)) {
-			persons.push_back(person);
-			return;
-		}
 	
-	std::cin.clear();
-	std::cin.ignore(10000, '\n');
-}
+	int hands;
+	int feet;
+	int eyes;
 
-}
+	void PrintType()
+	{
+		std::cout << "I am a human being!" << std::endl;
+	}
 
-void inputNewData(std::string message, std::string& data) {
-	std::cout << message << std::endl;
-	std::cin >> data;
-	std::cin.clear();
-	std::cin.ignore(10000, '\n');
-}
+private:
+
+};
+
+class Male : public Human
+{
+
+};
+
+class Female : public Human
+{
+
+};
 
 
 int main() {
-	
-	//Zadatak napraviti klasu osobe, sadržaja ime, prezime, adresa, oib;
-	//Konstrukor treba inicijalizirati sve membere osim oiba
-	//posebna metoda za setanje oiba jel valjan ili postojeæi, na temelju toga 
-	//namjestiti oib
-	//napraviti program koji omoguæuje izradu novih osoba
+	Human maleObject;
+	maleObject.hands = 2;
+	maleObject.feet = 2;
+	maleObject.eyes = 2;
 
-	//vektori - polja istog tipa, 
-	//CTR + R - rename svih varijabli istog imena
-
-	Person person = Person(name, lastName, adress);
-
-	std::vector<Person> persons;
-
-	bool valid = true;
-	while (valid) {
-		std::cout << "What do you want to do:" << std::endl;
-		std::cout << "1-Add new person" << std::endl;
-		std::cout << "2-Exit" << std::endl;
-
-		int choice;
-
-		std::cin >> choice;
-		std::cin.clear();
-		std::cin.ignore(10000, '\n');
-
-		switch (choice) {
-		case 1:
-			createPerson(persons);
-			break;
-		case 2:
-			valid = false;
-			break;
-		default:
-			break;
-
-		}
-	}
-
-	displayPersons(persons);
-	
+	maleObject.PrintType();
 	std::cin.get();
+
 }
